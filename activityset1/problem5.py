bafdaca5c6d68829b9f8289afd9ab5a76ce2950a
Divyashetty89 <94425033+Divyashetty89@users.noreply.github.com>
2022-04-26 08:44:36 +0000

    yet to complete

--- activityset1/problem5.py
+++ activityset1/problem5.py
@@ -1,1 +1,13 @@
-
+def computepay(h,r):
+ if h > 40:
+  p = 1.5 * r * (h - 40) + (40 *r)
+ else:
+    p = h * r
+    return p
+    
+hrs = input("Enter Hours:")
+hr = float(hrs)
+rate = input("Enter rate per hour:")
+rp = float(rate)
+p = computepay(hr,rp)
+print(p)
