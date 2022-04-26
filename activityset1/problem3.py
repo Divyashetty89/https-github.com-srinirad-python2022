165943ba5be317ffc411ea78c5b2c2f0dc282c48
Divyashetty89 <94425033+Divyashetty89@users.noreply.github.com>
2022-04-26 06:24:37 +0000

    Done

--- activityset1/problem3.py
+++ activityset1/problem3.py
@@ -1,12 +1,13 @@
 hours=input("Enter hours")
-h=float(hrs)
-rate=input("Enter rate")
+h=float(hours)
+rate=input("Enter rate per hour")
 r=float(rate)
 if h<=40:
-  pay=float(hours)*float(rate)
-print(pay)
+  pay=h*r
+  print(pay)
 else:
   eh=h-40
-  pay=eh(1.5*r)+40*r
-print(pay)
-  
+  p=40*r+eh*(1.5*r)
+  print(p)
+
+
