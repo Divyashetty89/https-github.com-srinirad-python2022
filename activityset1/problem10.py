aaecd8c3afdd0138513230f1f581f6c6c411cc64
Divyashetty89 <94425033+Divyashetty89@users.noreply.github.com>
2022-06-02 06:38:08 +0000

    Done

--- activityset1/problem10.py
+++ activityset1/problem10.py
@@ -1,1 +1,14 @@
+fhand = open("activityset1/mbox-short.txt")
+count = 0
+for line in fhand:
+    line = line.rstrip()
+    if line == "": 
+      continue
+    words = line.split()
+    if words[0] !="From": 
+      continue
+        
+    print(words[1])
+    count = count+1
 
+print ( count, "lines in the file with From as first word ")
--- activityset1/problem9.py
+++ activityset1/problem9.py
@@ -1,1 +1,13 @@
-
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+  ln=line.rstrip()
+  words=ln.split()
+for element in words:
+  if element in lst :
+    continue
+else :
+  lst.append(element)
+  lst.sort()
+  print(lst)
