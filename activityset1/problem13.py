412655f045ea26d67594d768dfe41a0efe9ce9ea
Divyashetty89 <94425033+Divyashetty89@users.noreply.github.com>
2022-06-10 08:16:21 +0000

    Done

--- activityset1/problem13.py
+++ activityset1/problem13.py
@@ -1,1 +1,11 @@
-
+import re
+sum = 0
+file = open('activityset1/pgm13.txt')
+for line in file:
+    numbers = re.findall('[0-9]+', line)
+    if not numbers:
+        continue
+    else:
+        for number in numbers:
+            sum +=int(number)
+print(sum)
